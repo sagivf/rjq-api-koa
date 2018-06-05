832029db1188fd253436309517a2f529982db7df
Sagiv Frankel <sagivf@gmail.com>
2018-06-05 14:15:52 +0300

    wip

--- examples/server.js
+++ examples/server.js
@@ -1,11 +1,13 @@
 const Koa = require('koa')
 const rjqApiKoa = require('../index')
+const cors = require('@koa/cors');
 
 const app = new Koa()
 
 const { PORT = 4000 } = process.env
 
 app
+  .use(cors())
   .use(rjqApiKoa())
   .listen(PORT)
 
